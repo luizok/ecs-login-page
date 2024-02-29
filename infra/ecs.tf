f334cc099f7298abf2ae431f23bef20d7a179ad0
Luiz Felipe <luizfelipesmaciel@gmail.com>
2024-02-29 13:56:56 -0300

    Using task vars

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -24,8 +24,8 @@ resource "aws_ecs_task_definition" "task" {
   family                   = "${var.project_name}-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = var.task_cpu
+  memory                   = var.task_memory
   execution_role_arn       = aws_iam_role.ecs_service.arn
   task_role_arn            = aws_iam_role.ecs_task.arn
 
